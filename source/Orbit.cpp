7de5444ce43bf8dd390682e8b34d99576dec91b3
Bart Swinnen <saribro@gmail.com>
2018-07-23 22:48:25 +0200

    Cleanup of Orbit class: - Include/define only what is needed, where it is needed. - Don't use 'using namespace' in header. - _getSpeed() functions don't use members, can be static. - Use initializer list for constructor. - Map specific Change() functions to general variant to reduce code duplication. - Prevent uninitialized variables in inclination function, reduce code duplication. - Spacing around operators and parameters for readability.

--- source/Orbit.cpp
+++ source/Orbit.cpp
@@ -2,16 +2,18 @@
 // Created by soldato_fantasma aka Soltasto on 22/07/2018.
 //
 
-
 #include "Orbit.h"
 
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+#define EARTH_RADIUS_M 6371000
+#define MU 3.986005e14
+
 //Constructor, sets the orbit parameters to begin with
-Orbit::Orbit(double const & perigee, double const & apogee, double const & inclination) {
-    _perigee                =   perigee;
-    _apogee                 =   apogee;
-    _inclination            =   inclination;
-    _total_delta_v_change   =   0;
-}
+Orbit::Orbit(double const & perigee, double const & apogee, double const & inclination)
+    : _perigee(perigee), _apogee(apogee), _inclination(inclination), _total_delta_v_change(0) {}
 
 //Returns the Apogee
 double Orbit::getApogee() const {
@@ -27,28 +29,34 @@ double Orbit::getPerigee() const {
 double Orbit::getInclination() const {
     return _inclination;
 }
+
 //Returns the SMA
 double Orbit::getSemiMajorAxis() const {
-    return ((_perigee+_apogee)/2) + EARTH_RADIUS_M;
+    return ((_perigee + _apogee) / 2) + EARTH_RADIUS_M;
 }
+
 //Returns the current orbit speed at a certain altitude
-double Orbit::getOrbitalSpeed(double const & distance) const {
-    return sqrt(MU * (  (2/(distance + EARTH_RADIUS_M))  -  (1/getSemiMajorAxis())  )  );
+double Orbit::getOrbitalSpeed(double const & altitude) const {
+    return sqrt(MU * ( (2 / (altitude + EARTH_RADIUS_M))  -  (1 / getSemiMajorAxis()) ) );
 }
+
 //returns the total delta v change since orbit declaration
 double Orbit::getTotalDeltaVChange() const {
     return _total_delta_v_change;
 }
+
 //Prints the Orbit data
 void Orbit::print() const {
-    cout << setprecision(4) << fixed << "Current Orbit: "
-         << _perigee/1000 << " km x "
-         << _apogee/1000 << " km x "
-         << _inclination << " degrees; " << endl
+    std::cout << std::setprecision(4) << std::fixed
+         << "Current Orbit: "
+         << _perigee / 1000 << " km x "
+         << _apogee / 1000 << " km x "
+         << _inclination << " degrees;\n"
          << "Apogee Speed: " << getOrbitalSpeed(_apogee)
          << " m/s; Perigee Speed: " << getOrbitalSpeed(_perigee)
-         << " m/s; delta v to this orbit: " << getTotalDeltaVChange() << endl << endl;
+         << " m/s; delta v to this orbit: " << getTotalDeltaVChange() << std::endl << std::endl;
 }
+
 //Performs an Apogee + Inclination change
 double Orbit::apogeeChange(double const & new_apogee, const double & new_inclination) {
     //Calculating the speeds before the burn
@@ -66,30 +74,18 @@ double Orbit::apogeeChange(double const & new_apogee, const double & new_inclina
     double v_next_along     =   _getSpeedAlong(v_next,_inclination);
 
     //Calculating the speed change
-    double delta_v_change   =   sqrt(pow(v_next_cross - v_before_cross,2) + pow(v_next_along - v_before_along,2)) ;
+    double delta_v_change   =   sqrt(pow(v_next_cross - v_before_cross, 2) + pow(v_next_along - v_before_along, 2));
 
     //Saving the aggregate speed change
     _total_delta_v_change   +=  delta_v_change;
     return delta_v_change;
 }
-//Performs an Apogee change
+
+//Performs an Apogee change at current inclination
 double Orbit::apogeeChange(double const & new_apogee) {
-    //Calculating the speed before the burn
-    double delta_v_before   =   getOrbitalSpeed(_perigee);
-
-    //Changing the orbit parameters after the burn
-    _apogee     =   new_apogee;
-
-    //Calculating the speed after the burn
-    double delta_v_next     =   getOrbitalSpeed(_perigee);
-
-    //Calculating the speed change
-    double delta_v_change   =   fabs(delta_v_next-delta_v_before);
-
-    //Saving the aggregate speed change
-    _total_delta_v_change   +=  delta_v_change;
-    return delta_v_change;
+    return apogeeChange(new_apogee, _inclination);
 }
+
 //Performs a Perigee + Inclination change
 double Orbit::perigeeChange(double const & new_perigee, const double & new_inclination) {
     //Calculating the speeds before the burn
@@ -107,85 +103,59 @@ double Orbit::perigeeChange(double const & new_perigee, const double & new_incli
     double v_next_along     =   _getSpeedAlong(v_next,_inclination);
 
     //Calculating the speed change
-    double delta_v_change   =   sqrt(pow(v_next_cross - v_before_cross,2) + pow(v_next_along - v_before_along,2)) ;
+    double delta_v_change   =   sqrt(pow(v_next_cross - v_before_cross, 2) + pow(v_next_along - v_before_along, 2));
 
     //Saving the aggregate speed change
     _total_delta_v_change   +=  delta_v_change;
     return delta_v_change;
 }
-//Performs a Perigee change
+
+//Performs a Perigee change at current inclination
 double Orbit::perigeeChange(double const & new_perigee) {
-    //Calculating the speed before the burn
-    double delta_v_before   =   getOrbitalSpeed(_apogee);
-
-    //Changing the orbit parameters after the burn
-    _perigee    =   new_perigee;
-
-    //Calculating the speed after the burn
-    double delta_v_next     =   getOrbitalSpeed(_apogee);
-
-    //Calculating the speed change
-    double delta_v_change   =   fabs(delta_v_next-delta_v_before);
-
-    //Saving the aggregate speed change
-    _total_delta_v_change   +=  delta_v_change;
-    return delta_v_change;
+    return perigeeChange(new_perigee, _inclination);
 }
+
 //performs an Inclination change
 double Orbit::inclinationChange(double const & new_inclination, double const & altitude) {
-    double delta_v_change, speed_along_before, speed_cross_before, speed_along_next, speed_cross_next;
+    if (new_inclination == _inclination)
+        return 0.;  // no delta-v needed
 
-    if(_inclination!=new_inclination) { //checking if the inclinations has really changed
-        if (altitude == _perigee) { //checking if the inclination change burn is at perigee
-
-            //Calculating the speeds before the burn
-            speed_along_before  =   _getSpeedAlong(getOrbitalSpeed(_perigee), _inclination);
-            speed_cross_before  =   _getSpeedCross(getOrbitalSpeed(_perigee), _inclination);
-
-            //Changing the orbit parameters after the burn
-            _inclination        =   new_inclination;
-
-            //Calculating the speeds after the burn
-            speed_along_next    =   _getSpeedAlong(getOrbitalSpeed(_perigee), _inclination);
-            speed_cross_next    =   _getSpeedCross(getOrbitalSpeed(_perigee), _inclination);
-
-            //Calculating the speed change
-            delta_v_change      =   sqrt(
-                    pow((speed_along_next - speed_along_before), 2) + pow((speed_cross_next - speed_cross_before), 2));
-
-        } else if (altitude == _apogee) { //checking if the inclination change burn is at apogee
-
-            //Calculating the speeds before the burn
-            speed_along_before  =   _getSpeedAlong(getOrbitalSpeed(_apogee), _inclination);
-            speed_cross_before  =   _getSpeedCross(getOrbitalSpeed(_apogee), _inclination);
-
-            //Changing the orbit parameters after the burn
-            _inclination        =   new_inclination;
-
-            //Calculating the speeds after the burn
-            speed_along_next    =   _getSpeedAlong(getOrbitalSpeed(_apogee), _inclination);
-            speed_cross_next    =   _getSpeedCross(getOrbitalSpeed(_apogee), _inclination);
-
-            //Calculating the speed change
-            delta_v_change      =   sqrt(
-                    pow((speed_along_next - speed_along_before), 2) + pow((speed_cross_next - speed_cross_before), 2));
-
-        } else {
-            //Program must end if the inclination change is not happening at perigee or at apogee
-            cout << "ERROR! Wrong usage of inclination change." << endl;
-            exit(EXIT_FAILURE);
-        }
+    double speed = 0.;
+    if (altitude == _perigee)       //checking if the inclination change burn is at perigee
+        speed = getOrbitalSpeed(_perigee);
+    else if (altitude == _apogee)   //checking if the inclination change burn is at apogee
+        speed = getOrbitalSpeed(_apogee);
+    else {  //Program must end if the inclination change is not happening at perigee or at apogee
+        std::cout << "ERROR! Wrong usage of inclination change." << std::endl;
+        exit(EXIT_FAILURE);
     }
+
+    //Calculating the speeds before the burn
+    double speed_along_before   = _getSpeedAlong(speed, _inclination);
+    double speed_cross_before   = _getSpeedCross(speed, _inclination);
+
+    //Changing the orbit parameters after the burn
+    _inclination    = new_inclination;
+
+    //Calculating the speeds after the burn
+    double speed_along_next     = _getSpeedAlong(speed, _inclination);
+    double speed_cross_next     = _getSpeedCross(speed, _inclination);
+
+    //Calculating the speed change
+    double delta_v_change       = sqrt(
+            pow((speed_along_next - speed_along_before), 2) + pow((speed_cross_next - speed_cross_before), 2));
 
     //Saving the aggregate speed change
     _total_delta_v_change   +=  delta_v_change;
     return delta_v_change;
 }
+
 //Returns the speed along the reference frame
-double Orbit::_getSpeedAlong(double const & speed, double const & inclination) const {
-    return speed * cos((inclination/180)*M_PI);
+double Orbit::_getSpeedAlong(double const & speed, double const & inclination) {
+    return speed * cos((inclination / 180) * M_PI);
 }
+
 //Returns the speed across the reference frame
-double Orbit::_getSpeedCross(double const & speed, double const & inclination) const {
-    return speed * sin((inclination/180)*M_PI);
+double Orbit::_getSpeedCross(double const & speed, double const & inclination) {
+    return speed * sin((inclination / 180) * M_PI);
 }
